34ae87d65b5a3cb071a513b0e823e87fdaba7208
elvitajoyce <elvitaserrao@gmail.com>
2021-03-26 00:24:30 +0530

    Adding changes

--- src/main/java/sensorval/SensorValidator.java
+++ src/main/java/sensorval/SensorValidator.java
@@ -2,30 +2,29 @@ package sensorval;
 
 import java.util.List;
 
-public class SensorValidator 
-{
-    public static boolean _give_me_a_good_name(double value, double nextValue, double maxDelta) {
-        if(nextValue - value > maxDelta) {
-            return false;
-        }
-        return true;
-    }
-    public static boolean validateSOCreadings(List<Double> values) {
-        int lastButOneIndex = values.size() - 1;
-        for(int i = 0; i < lastButOneIndex; i++) {
-            if(!_give_me_a_good_name(values.get(i), values.get(i + 1), 0.05)) {
-            return false;
-            }
-        }
-        return true;
-    }
-    public static boolean validateCurrentreadings(List<Double> values) {
-        int lastButOneIndex = values.size() - 1;
-        for(int i = 0; i < lastButOneIndex; i++) {
-            if(!_give_me_a_good_name(values.get(i), values.get(i + 1), 0.1)) {
-            return false;
-            }
-        }
-        return true;
-    }
+public class SensorValidator {
+	public static boolean isJumpGreaterThanMaxDelta(double value, double nextValue, double maxDelta) {
+		if (nextValue - value > maxDelta) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean validateReadings(List<Double> values, double maxDelta) {
+		if (values != null) {
+			return validateJumps(values, maxDelta);
+		}
+		return false;
+
+	}
+
+	private static boolean validateJumps(List<Double> values, double maxDelta) {
+		int lastButOneIndex = values.size() - 1;
+		for (int i = 0; i < lastButOneIndex; i++) {
+			if (!isJumpGreaterThanMaxDelta(values.get(i), values.get(i + 1), maxDelta)) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
